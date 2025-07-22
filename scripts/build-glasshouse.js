a33aa6d6471decc63da2bc209b71df52e4af092e
Elisa <tqelisanguyen@gmail.com>
2025-07-23 08:20:42 +1000

    Add snippet for translating content into html template

--- scripts/build-glasshouse.js
+++ scripts/build-glasshouse.js
@@ -2,3 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const marked = require('marked');
 
+const CONTENT_DIR = 'test-content';
+const TEMPLATE_FILE = '/gh-test-article.html';      
+
